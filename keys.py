4e0eb59e0679675cb169d098138870cb3eb6ec83
João Pedro Felicio Pereira <72157587+joaocabeca2@users.noreply.github.com>
2024-03-11 22:14:12 -0300

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,5 @@
+LINK = 'https://api.kanye.rest'
+HEADERS = {'Content-Type': 'application/json; charset=utf-8'}
+LOGIN = 'COLOQUE SEU EMAIL'
+SENHA = 'COLOQUE SUA SENHA'
+DESTINATARIOS = [COLOQUE OS EMAILS DE QUEM VOCÊ ENVIARÁ A FRASE]

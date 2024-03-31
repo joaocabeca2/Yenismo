f9d4c4c7cc28bd23af39dcc1e1c3b8203c876991
João Pedro Felicio Pereira <72157587+joaocabeca2@users.noreply.github.com>
2024-03-31 00:57:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
+from fastapi import FastAPI
+from random import choice
 import json
-import requests
-import smtplib
-from deep_translator import GoogleTranslator
-from email.message import EmailMessage
-from keys import HEADERS, LINK, LOGIN, SENHA, DESTINATARIOS
 
+app = FastAPI()
 
-def gerar_citacao_ye():
-  response = requests.get(LINK, headers=HEADERS)
-  frase = response.json()['quote']
-  tradutor = GoogleTranslator(source='en', target='pt')
-  return tradutor.translate(frase)
+@app.get('/')
+def homepage():
+    with open(file='api.json',mode='r') as jsonfile:
+        return json.load(jsonfile)
 
-mensagem = EmailMessage()
-mensagem['Subject'] = 'Uma frase de Kanye West para melhorar seu dia!'
-mensagem['From'] = LOGIN
-
-with smtplib.SMTP_SSL(host='smtp.gmail.com', port=465) as servidor:
-  servidor.login(LOGIN, SENHA)
-  for email in DESTINATARIOS:
-    mensagem['To'] = email
-    mensagem.set_content(gerar_citacao_ye())
-    servidor.send_message(mensagem)
-    del mensagem['To']
+@app.get('/frase')
+def pegar_frase_aleatoria():
+    with open('api.json','r') as jsonfile:
+        api_ye = json.load(jsonfile)
+        album_aleatorio = choice(list(api_ye.items()))[0]
+        musica_aleatoria = choice(list(api_ye[album_aleatorio].items()))[0]
+        return (album_aleatorio,musica_aleatoria,choice(api_ye[album_aleatorio][musica_aleatoria]))
